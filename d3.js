f2a329f8995615bc42432198b11d6f8a69186d35
james-owen <james@jamesowen.io>
2018-10-13 14:27:03 -0400

    timing

--- d3.js
+++ d3.js
@@ -3,16 +3,18 @@ const d3 = require('d3');
 const { PerformanceObserver, performance } = require('perf_hooks');
 
 const obs = new PerformanceObserver((items) => {
-  console.log(items.getEntries()[0].duration);
+  console.log(items.getEntries()[0]);
   performance.clearMarks();
 });
 obs.observe({ entryTypes: ['measure'] });
 
 performance.mark('A');
-doSomeLongRunningProcess(() => {
+function doSomeLongRunningProcess() {
   performance.mark('B');
-  performance.measure('A to B', 'A', 'B');
-});
+  performance.measure(performance.now(), 'A', 'B');
+};
+
+doSomeLongRunningProcess();
 
 const data = [
   { "route": "/registered", "startTime": 113759509.926442, "duration": 1.14386 },
@@ -33,7 +35,6 @@ console.log("perf origin: ", performance.timeOrigin);
 console.log("Process: ", process.hrtime());
 console.log("Perf.now + perf.timeOrigin?", data[0].startTime);
 console.log("performance.now + origin", performance.now() + performance.timeOrigin);
-console.log(format(data[0].startTime));
 
 // start graph at performance.now();
 
